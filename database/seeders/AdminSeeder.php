36fdc29ffcd7f7956b165359c056d3049a7ef282
Rico Pahlefi <101618011+ricopahlefi22@users.noreply.github.com>
2024-05-05 21:14:18 +0700

    Add AdminSeeder

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -15,6 +15,13 @@ class AdminSeeder extends Seeder
         Admin::factory(10)->create();
 
         Admin::factory()->create([
+            'name' => 'Dian Utari',
+            'email' => 'dianutari989@gmail.com',
+            'level' => "Super Admin",
+            'password' => bcrypt(12341234),
+        ]);
+
+        Admin::factory()->create([
             'name' => 'Rico Pahlefi',
             'email' => 'ricopahlefi22@gmail.com',
             'level' => "Super Admin",
